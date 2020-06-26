13ba1a6734e1adb4b8f5143e88b9ceba34bfc101
Will <you@example.com>
2020-06-26 15:22:02 +0100

    moved CSS constnts to own module (for easier style edits while still in vanilla JS)

--- docs/lib/sensor-display/sensor-display.js
+++ docs/lib/sensor-display/sensor-display.js
@@ -1,6 +1,7 @@
 import { getColourClassForAqi } from "./airquality-index.js"
 import { loadStreetNames } from "./streetnames.js"
 import { fetchMeasurements, checkReadingIsStale } from "./opensensemap.js"
+import * as css from "./styling-constants.js"
 /*
 Provisional vanilla JS to populate sensor readings direcly from OpenMapSense API
 Expects itemListContainer to exist - injects DOM objects inside of that
@@ -45,13 +46,15 @@ function createInfoBox(deviceName, defraAqi, measurements, latestDustReadingDate
     var colorClass = getColourClassForAqi(defraAqi, stale);
     var card = cardWithTitle(deviceName, colorClass);
     var values = document.createElement("DIV");
-    values.classList.add( "level", "my-5", "is-mobile");
+    values.classList.add(...css.READINGS_CLASSLIST);
     values.id = deviceName;
     if (showDetail.checked)
         measurements.filter(x=> x.name.startsWith('PM')).forEach(measurement =>
-            values.appendChild(sensorReading(...Object.values(measurement),  "has-text-right", "is-6")));
+            values.appendChild(sensorReading(...Object.values(measurement), css.READINGDETAIL_CLASSLIST)));
 
-    values.appendChild(sensorReading("Defra DAQI", undefined, "", stale && !showDetail.checked ? "-" : defraAqi))
+    values.appendChild(sensorReading("Defra DAQI", undefined, "", 
+                                     stale && !showDetail.checked ? "-" : defraAqi, undefined, 
+                                     css.READINGINDEX_CLASSLIST))
     card.appendChild(values);
     if (showDetail.checked)
         card.appendChild(footerWithTextItems([moment(latestDustReadingDate).format("ddd Do MMM, HH:mm")]));
@@ -61,25 +64,25 @@ function createInfoBox(deviceName, defraAqi, measurements, latestDustReadingDate
 
 function cardWithTitle(titleText, iconColorClass) {
     var card = document.createElement("DIV");
-    card.classList.add("card");
+    card.classList.add(...css.CARD_CLASSLIST);
     card.appendChild(cardHeaderWithTitle(titleText, iconColorClass));
     return card;
 }
 
 function cardHeaderWithTitle(titleText, iconColorClass) {
     var header = document.createElement("DIV");
-    header.classList.add("card-header");
+    header.classList.add(...css.CARDHEADER_CLASSLIST);
 
     var title = document.createElement("LABEL");
-    title.classList.add("card-header-title");
+    title.classList.add(...css.CARDHEADERTITLE_CLASSLIST);
     title.id = titleText + '-title';
     title.innerText = titleText;
 
     header.appendChild(title);
     var iconSpan = document.createElement("SPAN");
-    iconSpan.classList.add("card-header-icon", "is-size-1");
+    iconSpan.classList.add(...css.CARDICONCONTAINER_CLASSLIST);
     var icon = document.createElement("I");
-    icon.classList.add("fas", "fa-leaf", iconColorClass);
+    icon.classList.add(...css.CARDICON_CLASSLIST, iconColorClass);
     iconSpan.appendChild(icon);
     header.appendChild(iconSpan);
     return header;
@@ -87,29 +90,29 @@ function cardHeaderWithTitle(titleText, iconColorClass) {
 
 function footerWithTextItems(items) {
     var footer = document.createElement("DIV");
-    footer.classList.add("card-footer");
+    footer.classList.add(...css.CARDFOOTER_CLASSLIST);
     items.forEach(x => footer.appendChild(footerItemWithText(x)));
     return footer;
 }
 
 function footerItemWithText(text) {
     var footerContent = document.createElement("SPAN");
-    footerContent.classList.add("card-footer-item");
+    footerContent.classList.add(...css.CARDFOOTERITEM_CLASSLIST);
     footerContent.innerText = text;
     return footerContent;
 }
 
-function sensorReading(name, type, units, reading, readingTaken, alignmentClass, sizingClass) {
+function sensorReading(name, type, units, reading, readingTaken, valueClasslist) {
     var readingLine = document.createElement("DIV");
-    readingLine.classList.add("level-item", "has-text-centered");
+    readingLine.classList.add(...css.READING_CLASSLIST);
 
     var inner = document.createElement("DIV");
     var label = document.createElement("P");
-    label.classList.add("heading");
+    label.classList.add(...css.READINGLABEL_CLASSLIST);
     label.innerText = name;
 
     var value = document.createElement("P");
-    value.classList.add("title", alignmentClass, sizingClass);
+    value.classList.add(...valueClasslist);
     value.innerText = reading + units ;
 
     inner.appendChild(label);
---
+++ docs/lib/sensor-display/styling-constants.js
@@ -0,0 +1,16 @@
+
+export const CARD_CLASSLIST = ["card"];
+export const CARDHEADER_CLASSLIST = ["card-header"];
+export const CARDHEADERTITLE_CLASSLIST = ["card-header-title"];
+
+export const CARDICONCONTAINER_CLASSLIST = ["card-header-icon", "is-size-1"];
+export const CARDICON_CLASSLIST = ["fas", "fa-leaf"];
+
+export const READINGS_CLASSLIST = ["level", "my-5", "is-mobile"];
+export const READING_CLASSLIST = ["level-item", "has-text-centered"];
+export const READINGLABEL_CLASSLIST = ["heading"];
+export const READINGINDEX_CLASSLIST = ["title"];
+export const READINGDETAIL_CLASSLIST = ["title", "has-text-right", "is-6"];
+
+export const CARDFOOTER_CLASSLIST = ["card-footer"];
+export const CARDFOOTERITEM_CLASSLIST = ["card-footer-item"];
